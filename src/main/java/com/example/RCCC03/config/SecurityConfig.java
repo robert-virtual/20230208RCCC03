f4214ac320d638fe6fde76014ab77935d5fa6007
robert-virtual <robertocastill63@gmail.com>
2023-02-16 13:49:22 -0600

    restrict user creation endpoint

--- src/main/java/com/example/RCCC03/config/SecurityConfig.java
+++ src/main/java/com/example/RCCC03/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
                .csrf()
                .disable()
                .authorizeHttpRequests()
-               .requestMatchers("/auth/**")
+               .requestMatchers("/auth/login")
                .permitAll()
                .anyRequest()
                .authenticated()
