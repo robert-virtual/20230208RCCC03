671b250aee3f078fc2371e67f1ce3d305197a2e0
robert-virtual <robertocastill63@gmail.com>
2023-02-23 17:52:36 -0600

    create transaction not working

--- src/main/java/com/example/RCCC03/transaction/controller/TransactionController.java
+++ src/main/java/com/example/RCCC03/transaction/controller/TransactionController.java
@@ -1,10 +1,21 @@
 package com.example.RCCC03.transaction.controller;
 
+import com.example.RCCC03.config.BasicResponse;
+import com.example.RCCC03.transaction.model.Transaction;
+import com.example.RCCC03.transaction.service.TransactionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/transaction")
+@RequiredArgsConstructor
 public class TransactionController {
-
+    private final TransactionService transactionService;
+    @PostMapping("/create")
+    public BasicResponse<Transaction> create(@RequestBody Transaction body){
+        return transactionService.create(body);
+    }
 }
---
+++ src/main/java/com/example/RCCC03/transaction/service/TransactionService.java
@@ -0,0 +1,83 @@
+package com.example.RCCC03.transaction.service;
+
+import com.example.RCCC03.account.model.Account;
+import com.example.RCCC03.account.repository.AccountRepository;
+import com.example.RCCC03.auth.model.User;
+import com.example.RCCC03.auth.repository.UserRepository;
+import com.example.RCCC03.config.BasicResponse;
+import com.example.RCCC03.customer.model.Customer;
+import com.example.RCCC03.customer.repository.CustomerRepository;
+import com.example.RCCC03.transaction.model.Transaction;
+import com.example.RCCC03.transaction.model.TransactionStatus;
+import com.example.RCCC03.transaction.repository.TransactionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class TransactionService {
+    private final TransactionRepository transactionRepo;
+    private final CustomerRepository customerRepo;
+    private final UserRepository userRepo;
+    private final AccountRepository accountRepo;
+
+    public BasicResponse<Transaction> create(Transaction transaction) {
+        try {
+            String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+            User user = userRepo.findByEmail(userEmail).orElseThrow();
+            Customer customer = customerRepo.findById(user.getCustomerId()).orElseThrow();
+            if (
+                    customer.getAccounts().stream().anyMatch(
+                            account -> account.getAccount_number() == transaction.getAccount().getAccount_number()
+                    )
+            ) return BasicResponse
+                    .<Transaction>builder()
+                    .error("the account does not belong to the user requesting the action")
+                    .build();
+            Account source_account = transaction.getAccount();
+            double total_debit = transaction.getDetails().stream().mapToDouble(
+                    detail -> {
+                        accountRepo.findById(detail.getTarget_account()).map(target_account -> {
+                            double credit = Double.parseDouble(
+                                    target_account.getAvailable_balance()
+                            ) + Double.parseDouble(
+                                    detail.getAmount()
+                            );
+                            double held_balance = Double.parseDouble(target_account.getHeld_balance()) + credit;
+                            target_account.setHeld_balance(
+                                    Double.toString(held_balance)
+                            );
+                            return accountRepo.save(target_account);
+                        });
+                        return Double.parseDouble(detail.getAmount());
+                    }
+            ).sum();
+            double held_balance = Double.parseDouble(source_account.getHeld_balance()) - total_debit;
+            source_account.setHeld_balance(
+                    Double.toString(held_balance)
+            );
+            accountRepo.save(source_account);
+            transaction.setDate(LocalDateTime.now());
+            transaction.setStatus(
+                    TransactionStatus
+                            .builder()
+                            .id(1)
+                            .build()
+            );
+            return BasicResponse
+                    .<Transaction>builder()
+                    .data(transactionRepo.save(transaction))
+                    .build();
+
+        }catch (Exception e){
+            return BasicResponse
+                    .<Transaction>builder()
+                    .error(e.getMessage())
+                    .build();
+
+        }
+    }
+}
