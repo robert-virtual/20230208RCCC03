d1d5b207ab223bf3699b50d44ad99cedd4565d70
robert-virtual <robertocastill63@gmail.com>
2023-02-24 09:53:08 -0600

    create transaction  working

--- src/main/java/com/example/RCCC03/transaction/model/TransactionDetail.java
+++ src/main/java/com/example/RCCC03/transaction/model/TransactionDetail.java
@@ -1,9 +1,6 @@
 package com.example.RCCC03.transaction.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity(name = "transaction_detail")
@@ -11,6 +8,7 @@ import lombok.Data;
 public class TransactionDetail {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "bigint primary key identity")
     private long id;
     private long transaction_id;
     private long target_account;
--- src/main/java/com/example/RCCC03/transaction/service/TransactionService.java
+++ src/main/java/com/example/RCCC03/transaction/service/TransactionService.java
@@ -8,18 +8,23 @@ import com.example.RCCC03.config.BasicResponse;
 import com.example.RCCC03.customer.model.Customer;
 import com.example.RCCC03.customer.repository.CustomerRepository;
 import com.example.RCCC03.transaction.model.Transaction;
+import com.example.RCCC03.transaction.model.TransactionDetail;
 import com.example.RCCC03.transaction.model.TransactionStatus;
+import com.example.RCCC03.transaction.repository.TransactionDetailRepository;
 import com.example.RCCC03.transaction.repository.TransactionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TransactionService {
     private final TransactionRepository transactionRepo;
+    private final TransactionDetailRepository transactionDetailsRepo;
     private final CustomerRepository customerRepo;
     private final UserRepository userRepo;
     private final AccountRepository accountRepo;
@@ -35,28 +40,7 @@ public class TransactionService {
                     .<Transaction>builder()
                     .error("the account does not belong to the user requesting the action")
                     .build();
-            double total_debit = transaction.getDetails().stream().mapToDouble(
-                    detail -> {
-                        accountRepo.findById(detail.getTarget_account()).map(target_account -> {
-                            double credit = Double.parseDouble(
-                                    target_account.getAvailable_balance()
-                            ) + Double.parseDouble(
-                                    detail.getAmount()
-                            );
-                            double held_balance = Double.parseDouble(target_account.getHeld_balance()) + credit;
-                            target_account.setHeld_balance(
-                                    Double.toString(held_balance)
-                            );
-                            return accountRepo.save(target_account);
-                        });
-                        return Double.parseDouble(detail.getAmount());
-                    }
-            ).sum();
-            double held_balance = Double.parseDouble(source_account.getHeld_balance()) - total_debit;
-            source_account.setHeld_balance(
-                    Double.toString(held_balance)
-            );
-            accountRepo.save(source_account);
+
             transaction.setDate(LocalDateTime.now());
             transaction.setStatus(
                     TransactionStatus
@@ -64,9 +48,47 @@ public class TransactionService {
                             .id(1)
                             .build()
             );
+            List<TransactionDetail> details = transaction.getDetails();
+            transaction.setDetails(new ArrayList<>());
+            long transaction_id = transactionRepo.save(transaction).getId();
+            double total_debit = details.stream().mapToDouble(
+                    detail -> {
+                        return Double.parseDouble(detail.getAmount());
+                    }
+            ).sum();
+            details = details.stream().map(detail->{
+                detail.setTransaction_id(transaction_id);
+                return detail;
+            }).toList();
+
+            transactionDetailsRepo.saveAll(details);
+            //-----perform credit to target accounts--------
+            details.forEach(detail -> {
+                accountRepo.findById(detail.getTarget_account()).map(target_account -> {
+                    double credit = Double.parseDouble(
+                            target_account.getAvailable_balance()
+                    ) + Double.parseDouble(
+                            detail.getAmount()
+                    );
+                    double held_balance = Double.parseDouble(target_account.getHeld_balance()) + credit;
+                    target_account.setHeld_balance(
+                            Double.toString(held_balance)
+                    );
+                    return accountRepo.save(target_account);
+                });
+            });
+            // --------------------------------------------
+            // -------perform debit to source account-------
+            double held_balance = Double.parseDouble(source_account.getHeld_balance()) - total_debit;
+            source_account.setHeld_balance(
+                    Double.toString(held_balance)
+            );
+            accountRepo.save(source_account);
+            // --------------------------------------------
+            transaction.setDetails(details);
             return BasicResponse
                     .<Transaction>builder()
-                    .data(transactionRepo.save(transaction))
+                    .data(transaction)
                     .build();
 
         } catch (Exception e) {
